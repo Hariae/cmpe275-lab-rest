b603636005c06449954c1a973c13d382b24338b5
saumya2904 <saumya.iis@gmail.com>
2019-04-12 19:28:27 -0700

    Added jdbc driver

--- springmvc-helloworld/src/main/java/jbr/springmvc/RequestController.java
+++ springmvc-helloworld/src/main/java/jbr/springmvc/RequestController.java
@@ -1,17 +1,24 @@
 package jbr.springmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jbr.springmvc.dao.EmployeeDao;
+import jbr.springmvc.dao.EmployeeDaoImpl;
+
 @Controller
 @RequestMapping("/request")
 public class RequestController {
-	
+	@Autowired
+	public EmployeeDao empdao;
 	@RequestMapping(method=RequestMethod.GET) 
 	  @ResponseBody
 	  public String test() {
+		//EmployeeDaoImpl obj = new EmployeeDaoImpl();
+		empdao.register(null);
 		return "test-success";
 	}
 }
--- springmvc-helloworld/src/main/java/jbr/springmvc/dao/EmployeeDaoImpl.java
+++ springmvc-helloworld/src/main/java/jbr/springmvc/dao/EmployeeDaoImpl.java
@@ -1,5 +1,8 @@
 package jbr.springmvc.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +18,10 @@ public class EmployeeDaoImpl implements EmployeeDao{
 	  JdbcTemplate jdbcTemplate;
 	  
 	public void register(Employee employee) {
-		 String sql = "select * from users where 1=1";
+		 String sql = "select * from users";
+		 System.out.println("INSIDE" + jdbcTemplate);
+      List<Map<String, Object>> employees = jdbcTemplate.queryForList(sql);
+		    System.out.println(employees);
 		 
 		 
 	}
